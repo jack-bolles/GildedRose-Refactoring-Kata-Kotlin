cdbdba9e3b3d3b36716aebed977ad854811d660f
jack-bolles <65552523+jack-bolles@users.noreply.github.com>
2021-11-02 17:04:09 +0000

    simplify the fixture

--- src/test/kotlin/com/gildedrose/TexttestFixture.kt
+++ src/test/kotlin/com/gildedrose/TexttestFixture.kt
@@ -1,71 +1,72 @@
 package com.gildedrose
 
 fun main(args: Array<String>) {
-    `initial fixture`(args)
-    gigo()
+    updateQualityOf(gigo)
 }
 
-fun gigo() {
-    println("GIGO!")
+private val gigo: Array<Item>
+    get() {
+        return arrayOf(
+            Item("+5 Dexterity Vest", 10, 20),
+            Item("Aged Brie", 2, 0),
+            Item("Elixir of the Mongoose", 5, 7),
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            Item("Conjured Mana Cake", 3, 6)
+        )
+    }
 
-    val items = arrayOf(
-        Item("+5 Dexterity Vest", 10, 20),
-        Item("Aged Brie", 2, 0),
-        Item("Elixir of the Mongoose", 5, 7),
-        Item("Sulfuras, Hand of Ragnaros", 0, 80),
-        Item("Sulfuras, Hand of Ragnaros", -1, 80),
-        Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-        Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-        // this conjured item does not work properly yet
-        Item("Conjured Mana Cake", 3, 6)
-    )
-
+fun updateQualityOf(arrayOfItems: Array<Item>) {
     var dayCount = 0
-    val app = GildedRose(items)
-    `report days stock`(dayCount, items)
+    val app = GildedRose(arrayOfItems)
+
+    var actual = `report days stock`(dayCount, arrayOfItems)
+    if (gigoDayZero == actual) println("day Zero as expected")
 
     app.updateQuality()
-    `report days stock`(++dayCount, items)
+    actual = `report days stock`(++dayCount, arrayOfItems)
+    if (gigoDayOne == actual) println("day One as expected")
 }
 
-private fun `report days stock`(dayCount: Int, items: Array<Item>) {
-    println("-------- day $dayCount --------")
-    println("name, sellIn, quality")
-    val stringBeforeUpdate = items.joinToString("\n")
-    println(stringBeforeUpdate)
+private fun `report days stock`(dayCount: Int, items: Array<Item>): String {
+    return "-------- day $dayCount --------" + "\n" +
+            "name, sellIn, quality" + "\n" +
+            items.joinToString("\n")
 }
 
-private fun `initial fixture`(args: Array<String>) {
-    println("OMGHAI!")
-
-    val items = arrayOf(
-        Item("+5 Dexterity Vest", 10, 20), //
-        Item("Aged Brie", 2, 0), //
-        Item("Elixir of the Mongoose", 5, 7), //
-        Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-        Item("Sulfuras, Hand of Ragnaros", -1, 80),
-        Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-        Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-        // this conjured item does not work properly yet
-        Item("Conjured Mana Cake", 3, 6)
-    )
-
-    val app = GildedRose(items)
-
-    var days = 2
-    if (args.isNotEmpty()) {
-        days = Integer.parseInt(args[0]) + 1
+private val gigoDayZero: String
+    get() {
+        return """-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+Aged Brie, 2, 0
+Elixir of the Mongoose, 5, 7
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 15, 20
+Backstage passes to a TAFKAL80ETC concert, 10, 49
+Backstage passes to a TAFKAL80ETC concert, 5, 49
+Conjured Mana Cake, 3, 6"""
+            .trimIndent()
     }
 
-    for (i in 0 until days) {
-        println("-------- day $i --------")
-        println("name, sellIn, quality")
-        for (item in items) {
-            println(item)
-        }
-        println()
-        app.updateQuality()
+private val gigoDayOne: String
+    get() {
+        return """-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Conjured Mana Cake, 2, 5"""
+            .trimIndent()
     }
-}
+
