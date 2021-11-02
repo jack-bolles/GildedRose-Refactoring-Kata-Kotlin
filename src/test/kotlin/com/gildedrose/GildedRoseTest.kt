6a639c6005519d9954aa3dd7fc71050369844c28
jack-bolles <65552523+jack-bolles@users.noreply.github.com>
2021-11-02 19:49:17 +0000

    Backstage passes ages correctly

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun `passes age to zero after the concert`() {
+    fun `passes quality falls to zero after the concert`() {
         val dayZeroPasses = arrayOf(
             Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
         )
@@ -20,8 +20,61 @@ internal class GildedRoseTest {
         assertEquals(nextDayPasses[0], app.items[0])
     }
 
-    //Quality increases by 2 when there are 10 days or less and
-    // by 3 when there are 5 days or less but
+    @Test
+    fun `passes quality increases by 2 when there are 10 days or less`() {
+        val dayZeroPasses = arrayOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 12, 30),
+            Item("Backstage passes to a TAFKAL80ETC concert", 11, 30),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 30),
+        )
 
+        val nextDayPasses = arrayOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 11, 29),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 32),
+            Item("Backstage passes to a TAFKAL80ETC concert", 9, 32),
+        )
+
+        val app = GildedRose(dayZeroPasses)
+        app.updateQuality()
+        assertEquals(nextDayPasses[0], app.items[0])
+        assertEquals(nextDayPasses[1], app.items[1])
+        assertEquals(nextDayPasses[2], app.items[2])
+    }
+
+    @Test
+    fun `passes quality increases by 3 when there are 5 days or less `() {
+        val dayZeroPasses = arrayOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 30),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 30),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 30),
+        )
+
+        val nextDayPasses = arrayOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 32),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 33),
+            Item("Backstage passes to a TAFKAL80ETC concert", 3, 33),
+        )
+
+        val app = GildedRose(dayZeroPasses)
+        app.updateQuality()
+        assertEquals(nextDayPasses[0], app.items[0])
+        assertEquals(nextDayPasses[1], app.items[1])
+        assertEquals(nextDayPasses[2], app.items[2])
+    }
+
+    @Test
+    fun `quality cant exceed 50`() {
+        val dayZeroPasses = arrayOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+        )
+
+        val nextDayPasses = arrayOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
+        )
+
+        val app = GildedRose(dayZeroPasses)
+        app.updateQuality()
+        assertEquals(nextDayPasses[0], app.items[0])
+    }
 
 }
