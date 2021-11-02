52d2be2ad1f0ba3548b12daf9daa43c57d3a01fa
jack-bolles <65552523+jack-bolles@users.noreply.github.com>
2021-11-02 20:31:50 +0000

    aging more deterministic rather than cascading through; bit of a tidy

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,5 +1,6 @@
 package com.gildedrose
 
+import java.lang.Integer.max
 import java.lang.Integer.min
 
 class GildedRose(var items: Array<Item>) {
@@ -24,55 +25,45 @@ class GildedRose(var items: Array<Item>) {
     }
 
     private fun updateBrie(item: Item) {
-        betterWithAge(item, 1)
         item.sellIn = item.sellIn - 1
-        if (item.sellIn < 0) {
-            betterWithAge(item, 1)
+
+        when {
+            item.sellIn < 0 -> {
+                age(item, 2)
+            }
+            else -> {
+                age(item, 1)
+            }
         }
     }
 
     private fun updatePass(item: Item) {
         item.sellIn = item.sellIn - 1
 
-        //Quality increases by 2 when there are 10 days or less and
-        //by 3 when there are 5 days or less
-        var qualityAdjustment = 1
-        if (item.sellIn <= 10) {
-            qualityAdjustment++
-        }
-        if (item.sellIn <= 5) {
-            qualityAdjustment++
-        }
+        var qualityAdjustment: Int
+        when {
+            item.sellIn < 0 -> -item.quality
+            item.sellIn <= 5 -> 3
+            item.sellIn <= 10 -> 2
+            else -> 1
+        }.also { qualityAdjustment = it }
 
-        //Quality drops to 0 after the concert
-        if (item.sellIn < 0) {
-            qualityAdjustment = -item.quality
-        }
-
-        betterWithAge(item, qualityAdjustment)
+        age(item, qualityAdjustment)
     }
 
     private fun updateLegend(item: Item) { /*do nothing */
     }
 
     private fun update(item: Item) {
-        worseWithAge(item)
-
         //tbc - can this be negative? what does that mean ??
         item.sellIn = item.sellIn - 1
-
+        age(item, -1)
     }
 
-    private fun worseWithAge(item: Item) {
-        if (item.quality > 0) {
-            item.quality = item.quality + -1
-        }
-    }
-
-    private fun betterWithAge(item: Item, amountToAge: Int) {
-        if (item.quality < 50) {
-            item.quality = min(50, item.quality + amountToAge)
-        }
+    private fun age(item: Item, amountToAge: Int) {
+        val quality = min(50, item.quality + amountToAge)
+        item.quality = min(50, quality)
+        item.quality = max(0, quality)
     }
 
 }
