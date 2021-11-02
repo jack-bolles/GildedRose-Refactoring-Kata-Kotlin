ea9eaeddd0bfe16e1d772841dd51340e439545c2
jack-bolles <65552523+jack-bolles@users.noreply.github.com>
2021-11-02 17:27:50 +0000

    change conditionals to assert match instead of negate match; isolate behaviour item.name

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,51 +3,58 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+        items.indices.forEach { i ->
+            val item = items[i]
+            val name = item.name
+            when (name) {
+                "Aged Brie" -> if (item.quality < 50) {
+                    item.quality = item.quality + 1
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                "Backstage passes to a TAFKAL80ETC concert" -> if (item.quality < 50) {
+                    item.quality = item.quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
+                    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
                     }
                 }
+                "Sulfuras, Hand of Ragnaros" -> { //do nothing
+                }
+                else -> if (item.quality > 0) {
+                    item.quality = item.quality - 1
+                }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            when (name) {
+                "Sulfuras, Hand of Ragnaros" -> {
+                }
+                else -> {
+                    item.sellIn = item.sellIn - 1
+                }
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                when (name) {
+                    "Backstage passes to a TAFKAL80ETC concert" -> {
+                        item.quality = item.quality - item.quality
+                    }
+                    "Sulfuras, Hand of Ragnaros" -> {
+                    }
+                    "Aged Brie" -> {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        } else {
+                            if (item.quality > 0) {
+                                item.quality = item.quality - 1
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
                     }
                 }
             }
