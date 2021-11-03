31ef25dcb04002d62fc05f41a58366c17f255942
jack-bolles <65552523+jack-bolles@users.noreply.github.com>
2021-11-03 15:42:08 +0000

    collect all of backstage passes uniqueness inone place

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -4,6 +4,25 @@ data class Item(val name: String, val sellIn: Int, val quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
+
+    object Pass {
+        internal fun incrementQuality(item: Item, daysOut: Int): Int {
+            val daysToSell = item.sellIn
+            var totalIncrement = 0
+            (1 until daysOut + 1).forEach { i ->
+                totalIncrement += increment(daysToSell - i, item)
+            }
+            return totalIncrement
+        }
+
+        private fun increment(sellIn: Int, item: Item): Int {
+            return if (sellIn < 0) -item.quality
+            else if (sellIn <= 5) 3
+            else if (sellIn <= 10) 2
+            else 1
+        }
+
+    }
 }
 
 fun age(item: Item, daysOut: Int): Item {
@@ -22,37 +41,20 @@ private fun remainingDaysToSell(item: Item, daysOut: Int): Int {
 
 private fun calculateQuality(item: Item, daysOut: Int): Int {
     return when {
-        item.name == "Backstage passes to a TAFKAL80ETC concert" -> determineQuality(
-            item.quality, incrementQuality(item, daysOut)
-        )
+        item.name == "Backstage passes to a TAFKAL80ETC concert" ->
+            boundedQuality(item.quality + Item.Pass.incrementQuality(item, daysOut))
 
         item.name == "Sulfuras, Hand of Ragnaros" -> item.quality
-        item.name == "Aged Brie" -> determineQuality(item.quality, daysOut)
-        item.name.startsWith("Conjured") -> determineQuality(item.quality, -2 * daysOut)
-        else -> determineQuality(item.quality, -1 * daysOut)
+        item.name == "Aged Brie" -> boundedQuality(item.quality + daysOut)
+        item.name.startsWith("Conjured") ->
+            boundedQuality(item.quality + -2 * daysOut)
+        else -> boundedQuality(item.quality + -1 * daysOut)
     }
 }
 
-private fun incrementQuality(item: Item, daysOut: Int): Int {
-    val daysToSell = item.sellIn
-    var totalIncrement = 0
-    (1 until daysOut + 1).forEach { i ->
-        totalIncrement += increment(daysToSell - i, item)
-    }
-    return totalIncrement
-}
-
-private fun increment(sellIn: Int, item: Item): Int {
-    return if (sellIn < 0) -item.quality
-    else if (sellIn <= 5) 3
-    else if (sellIn <= 10) 2
-    else 1
-}
-
-private fun determineQuality(initialQuality: Int, amountToAge: Int, hasMax: Boolean = true): Int {
-    var quality = initialQuality + amountToAge
-    quality = Integer.max(0, quality)
-    if (hasMax) quality = Integer.min(50, quality)
-
-    return quality
+private fun boundedQuality(rawQuality: Int): Int {
+    return Integer.min(
+        50,
+        Integer.max(0, rawQuality)
+    )
 }
