c60f91425042742a1fdb4c5f511c388618322e1f
jack-bolles <65552523+jack-bolles@users.noreply.github.com>
2021-11-03 10:16:22 +0000

    tidy

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -6,7 +6,8 @@ data class Item(val name: String, val sellIn: Int, val quality: Int) {
     }
 }
 
-//this begs some typing, but at this point we only know that certain behaviours are keyed off of specific string values
+//this begs some typing, but at this point we only know that certain behaviours
+// are keyed off of specific string values
 fun age(item: Item): Item {
     return if (item.name == "Aged Brie") {
         ageBrie(item)
@@ -22,24 +23,22 @@ fun age(item: Item): Item {
 private fun ageBrie(item: Item): Item {
     return item.copy(
         sellIn = item.sellIn - 1,
-        quality = determineQuality(item, 1)
+        quality = determineQuality(item.quality, 1)
     )
 }
 
 private fun agePass(item: Item): Item {
     val sellIn = item.sellIn - 1
-
-    val qualityAdjustment: Int = when {
-        sellIn < 0 -> -item.quality
-        sellIn <= 5 -> 3
-        sellIn <= 10 -> 2
-        else -> 1
-    }
-
-    val quality = determineQuality(item, qualityAdjustment)
     return item.copy(
         sellIn = sellIn,
-        quality = quality
+        quality = determineQuality(
+            item.quality, when {
+                sellIn < 0 -> -item.quality
+                sellIn <= 5 -> 3
+                sellIn <= 10 -> 2
+                else -> 1
+            }
+        )
     )
 }
 
@@ -50,19 +49,19 @@ private fun ageLegend(item: Item): Item { /*do nothing */
 fun ageConjured(item: Item): Item {
     return item.copy(
         sellIn = item.sellIn - 1,
-        quality = determineQuality(item, -2)
+        quality = determineQuality(item.quality, -2)
     )
 }
 
 private fun ageGeneric(item: Item): Item {
     return item.copy(
         sellIn = item.sellIn - 1,
-        quality = determineQuality(item, -1)
+        quality = determineQuality(item.quality, -1)
     )
 }
 
-private fun determineQuality(item: Item, amountToAge: Int, hasMax: Boolean = true): Int {
-    var quality = item.quality + amountToAge
+private fun determineQuality(initialQuality: Int, amountToAge: Int, hasMax: Boolean = true): Int {
+    var quality = initialQuality + amountToAge
     quality = Integer.max(0, quality)
     if (hasMax) quality = Integer.min(50, quality)
 
